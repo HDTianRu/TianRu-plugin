ccf6992a393d107fd26e2bb30100bd7408b057cf
HDTianRu <3291691454@qq.com>
2023-07-19 11:32:11 +0800

    在没有xiaoyao-cvs-plugin的情况下可以用抽卡记录链接更新

--- apps/提瓦特小助手抽卡记录.js
+++ apps/提瓦特小助手抽卡记录.js
@@ -3,8 +3,10 @@ import fetch from 'node-fetch'
 import lodash from 'lodash'
 import GachaLog from '../../genshin/model/gachaLog.js'
 import moment from 'moment'
-import User from "../../xiaoyao-cvs-plugin/model/user.js"
 import common from '../../../lib/common/common.js'
+import { _path } from '../models/path.js'
+import fs from 'node:fs'
+import {getUid} from '../models/utils.js'
 
 export class QRCode extends plugin {
   constructor() {
@@ -15,7 +17,7 @@ export class QRCode extends plugin {
       priority: 10,
       rule: [
         {
-          reg: "^#?(获取|更新)(提瓦特)?小助手(抽卡|祈愿)?(记录|历史)",
+          reg: "^#?(获取|更新)(提瓦特)?小助手(抽卡|祈愿)?(记录|历史)( *|\n*)(https.*)?",
           fnc: "getGcLog"
         }
       ]
@@ -35,49 +37,122 @@ export class QRCode extends plugin {
   }
 
   async getGcLog(e) {
-  let user = new User(e);
-  await user.cookie(e)
-  let authkey = await getAuthKey(e, user)
-  if (!authkey) {
-    return true;
+    var authkey
+    let urlRet = /https.*/
+    let ret = urlRet.exec(this.e.msg)
+    if (ret) {
+      this.e.uid = await getUid(this.e)
+      this.e.msg = ret[0]
+      this.logUrl(e)
+      return true
+    } else if (fs.existsSync(`${_path}/plugins/xiaoyao-cvs-plugin`)) {
+      try {
+        const User = (await import("../../xiaoyao-cvs-plugin/model/user.js")).default
+        let user = new User(e);
+        await user.cookie(e)
+        authkey = await getAuthKey(e, user)
+        if (!authkey) {
+          return true;
+        }
+      } catch (err) {
+        logger.error(err)
+      }
+    } else {
+      if (!this.e.uid) {
+        this.e.uid = e?.runtime?.user?._regUid || await getUid(this.e)
+      }
+      let gacha = new GachaLog(e)
+      gacha.uid = this.e.uid
+      /** 获取authkey */
+      authkey = await redis.get(`${gacha.urlKey}${gacha.uid}`)
+      if (!authkey) {
+        this.e.reply("请私聊发送抽卡记录链接",true)
+        this.setContext('logUrl')
+        return true
+      }
+      
+      /** 调一次接口判断是否有效 */
+      let res = await gacha.logApi({ gacha_type: 301, authkey, region: gacha.getServer() })
+
+      /** key过期，或者没有数据 */
+      if (res.retcode !== 0 || !res?.data?.list || res.data.list.length <= 0) {
+        logger.debug(`${this.e.logFnc} ${res.message || 'error'}`)
+        this.e.reply("请私聊发送抽卡记录链接",true)
+        this.setContext('logUrl')
+        return true
+      }
+    }
+    if(!this.e.region) this.e.region = getServer(this.e.uid)
+    this.e.authkey = authkey
+    this._getGcLog(e)
+    return true
   }
-    if (!e.uid) {
-    e.uid = e?.runtime?.user?._regUid
+  
+  async logUrl (e) {
+    if (!this.e.uid) {
+      this.e.uid = e?.runtime?.user?._regUid || await getUid(this.e)
+    }
+    let gacha = new GachaLog(e)
+    gacha.uid = this.e.uid
+    let url = this.e.msg
+    let param = gacha.dealUrl(url)
+    if (!param) {
+      this.finish('logUrl')
+      return
+    }
+    if (!await gacha.checkUrl(param)) {
+      this.e.reply("链接错误")
+      this.finish('logUrl')
+      return
+    }
+    this.e.authkey = param.authkey
+    this.e.region = param.region
+    this._getGcLog(e)
+    this.finish('logUrl')
   }
-  let url = "https://www.lelaer.com/outputGacha.php"
-    let gachaURL = encodeURIComponent(`https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=301&gacha_id=fecafa7b6560db5f3182222395d88aaa6aaac1bc&timestamp=${Math.floor(Date.now() / 1000)}&lang=zh-cn&device_type=mobile&plat_type=ios&region=${e.region}&authkey=${encodeURIComponent(authkey)}&game_biz=hk4e_cn&gacha_type=301&page=1&size=5&end_id=0`)
-    let body = `uid=${e.uid}&gachaurl=${gachaURL}&lang=zh-Hans`
+
+  async _getGcLog (e) {
+    if (!this.e.authkey) return true
+    if (!this.e.uid) {
+      this.e.uid = e?.runtime?.user?._regUid || await getUid(this.e)
+    }
+    logger.mark(this.e.uid)
+    let gachaURL = encodeURIComponent(`https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog?authkey_ver=1&sign_type=2&auth_appid=webview_gacha&init_type=301&gacha_id=fecafa7b6560db5f3182222395d88aaa6aaac1bc&timestamp=${Math.floor(Date.now() / 1000)}&lang=zh-cn&device_type=mobile&plat_type=ios&region=${this.e.region}&authkey=${encodeURIComponent(this.e.authkey)}&game_biz=hk4e_cn&gacha_type=301&page=1&size=5&end_id=0`)
+    let url = "https://www.lelaer.com/outputGacha.php"
+    let body = `uid=${this.e.uid}&gachaurl=${gachaURL}&lang=zh-Hans`
     let param = {
-    headers: {
-      "Host": "www.lelaer.com",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    timeout: 10000,
-    method: 'post',
-    body: body
-  }
+      headers: {
+        "Host": "www.lelaer.com",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      timeout: 10000,
+      method: 'post',
+      body: body
+    }
     let response = await fetch(url, param)
     let json = await response.json()
-    if (!json || json?.code == 400) {
-      await e.reply("更新失败")
+    let list = json?.list
+    if (!json || !list) {
+      await this.e.reply(`更新失败 ${json?.result}`)
+      logger.mark(json)
       return true
     }
-    if (json.list.length <= 0) {
-      await e.reply("\"提瓦特小助手\"抽卡记录为空")
+    if (!list?.length || list?.length <= 0) {
+      await this.e.reply("\"提瓦特小助手\"抽卡记录为空")
       return true
     }
-    let data = dealJson(json.list)
-    if (!data) return false
+    let data = dealJson(list,e)
+    if (!data) return true
     /** 保存json */
     let msg = []
-    let gachLog = new GachaLog(this.e)
-    gachLog.uid = e.uid
+    let gachaLog = new GachaLog(this.e)
+    gachaLog.uid = this.e.uid
     for (let type in data) {
       if (!this.typeName[type]) continue
-      gachLog.type = type
+      gachaLog.type = type
       let log = gachaLog.readJson()
       let finalJson = mergeJson(log.list, data[type])
-      gachLog.writeJson(finalJson)
+      gachaLog.writeJson(finalJson)
 
       msg.push(`${this.typeName[type]}记录：${data[type].length}条，现共${finalJson.length}条`)
     }
@@ -89,7 +164,7 @@ export class QRCode extends plugin {
 
 async function getAuthKey(e, user) {
   if (!e.uid) {
-    e.uid = e?.runtime?.user?._regUid
+    e.uid = e?.runtime?.user?._regUid || getUid(e)
   }
   e.region = getServer(e.uid)
   let authkeyrow = await user.getData("authKey", {});
@@ -119,7 +194,7 @@ function getServer(uid) {
   return 'cn_gf01'
 }
 
-function dealJson (list) {
+function dealJson (list,e) {
   let data = {}
 
   /** 必要字段 */
@@ -127,7 +202,7 @@ function dealJson (list) {
 
   for (let v of reqField) {
     if (!list[0][v]) {
-      this.e.reply(`json文件内容错误：缺少必要字段${v}`)
+      e.reply(`json文件内容错误：缺少必要字段${v}`)
       return false
     }
   }

f15e1b09724edbd73d02de791d795ea0f10b0c3d
HDTianRu <3291691454@qq.com>
2024-07-17 21:45:02 +0800

    fix code.js

--- apps/code.js
+++ apps/code.js
@@ -1,6 +1,8 @@
 import plugin from '../../../lib/plugins/plugin.js'
 import fetch from 'node-fetch'
-import { makeForwardMsg } from '../models/utils.js'
+import {
+  makeForwardMsg
+} from '../models/utils.js'
 
 export class Code extends plugin {
   constructor () {
@@ -13,14 +15,12 @@ export class Code extends plugin {
       event: 'message',
       /** 优先级，数字越小等级越高 */
       priority: 5000,
-      rule: [
-      {
+      rule: [{
         /** 命令正则匹配 */
         reg: `^(#|\.)?code .*(\r|\n)*`,
         /** 执行方法 */
         fnc: 'code'
-      }
-      ]
+      }]
     })
   }
 
@@ -30,103 +30,103 @@ export class Code extends plugin {
     let a = /\r|\n/.exec(text).index;
     let lang = text.substring(0, a).substring(text.indexOf(" ")+1).toLowerCase();
     let command = text.substring(a + 1);
-    
+
     var FileName;
     switch (lang) {
       case "bash":
-        lang="bash";
-        FileName="sh";
+        lang = "bash";
+        FileName = "sh";
         break;
       case "sh":
-        lang="bash";
-        FileName="sh";
+        lang = "bash";
+        FileName = "sh";
         break;
       case "py":
-        lang="python";
-        FileName="py";
+        lang = "python";
+        FileName = "py";
         break;
       case "python":
-        lang="python";
-        FileName="py";
+        lang = "python";
+        FileName = "py";
         break;
       case "c":
-        lang="c";
-        FileName="c";
+        lang = "c";
+        FileName = "c";
         break;
       case "c++":
-        lang="cpp";
-        FileName="cpp";
+        lang = "cpp";
+        FileName = "cpp";
         break;
       case "cpp":
-        lang="cpp";
-        FileName="cpp";
+        lang = "cpp";
+        FileName = "cpp";
         break;
       case "js":
-        lang="javascript";
-        FileName="js";
+        lang = "javascript";
+        FileName = "js";
         break;
       case "javascript":
-        lang="javascript";
-        FileName="js";
+        lang = "javascript";
+        FileName = "js";
         break;
       case "kt":
-        lang="kotlin";
-        FileName="kt";
+        lang = "kotlin";
+        FileName = "kt";
         break;
       case "kotlin":
-        lang="kotlin";
-        FileName="kt";
+        lang = "kotlin";
+        FileName = "kt";
         break;
       case "lua":
-        lang="lua";
-        FileName="lua";
+        lang = "lua";
+        FileName = "lua";
         break;
       case "go":
-        lang="go";
-        FileName="go";
+        lang = "go";
+        FileName = "go";
         break;
       case "golang":
-        lang="go";
-        FileName="go";
+        lang = "go";
+        FileName = "go";
         break;
       case "c#":
-        lang="csharp";
-        FileName="cs";
+        lang = "csharp";
+        FileName = "cs";
         break;
       case "java":
-        lang="java";
-        FileName="java";
+        lang = "java";
+        FileName = "java";
         break;
       case "php":
-        lang="php";
-        FileName="php";
+        lang = "php";
+        FileName = "php";
         break;
       case "groovy":
-        lang="groovy";
-        FileName="groovy";
+        lang = "groovy";
+        FileName = "groovy";
         break;
       case "rust":
-        lang="rust";
-        FileName="rs";
+        lang = "rust";
+        FileName = "rs";
         break;
       case "typescript":
-        lang="typescript";
-        FileName="ts";
+        lang = "typescript";
+        FileName = "ts";
         break;
       case "ts":
-        lang="typescript";
-        FileName="ts";
+        lang = "typescript";
+        FileName = "ts";
         break;
       case "perl":
-        lang="perl";
-        FileName="pl";
+        lang = "perl";
+        FileName = "pl";
         break;
       default:
-        this.reply("未知语言" + lang,true);
+        this.reply("未知语言" + lang, true);
         return;
         break;
     }
-  
+
     let postData = JSON.stringify({
       files: [{
         name: "main."+FileName,
@@ -135,22 +135,33 @@ export class Code extends plugin {
       stdin: "",
       command: ""
     });
-  
-    fetch("https://glot.io/run/" + lang + "?version=latest", {
+
+    let postData = JSON.stringify({
+      files: [{
+        name: "main." + FileName,
+        content: command
+      }],
+      stdin: "",
+      command: ""
+    });
+
+    try {
+      const response = await fetch("https://glot.io/run/" + lang + "?version=latest", {
         method: "POST",
         headers: {
           "Content-Type": "application/x-www-form-urlencoded"
         },
         body: postData
-      })
-      .then(response => response.json())
-      .then(async data => {
-        let out = data.stdout + data.stderr + data.error;
-        out = out || "执行成功，但是没有输出";
-        //this.reply(out.replace(/\\n/g, "\n"),true);
-        let msg = await makeForwardMsg(e, out, "执行结果")
-        this.reply(msg)
-      })
-      .catch(error => console.error(error));
+      });
+
+      const data = await response.json();
+      let out = data.stdout + data.stderr + data.error;
+      out = out || "执行成功，但是没有输出";
+
+      let msg = await makeForwardMsg(e, out);
+      this.reply(msg);
+    } catch (error) {
+      logger.error(error);
+    }
   }
 }

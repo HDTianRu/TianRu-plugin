1de0bc6cf1fcaddb3870ae813b2129369701fa7c
HDTianRu <3291691454@qq.com>
2025-07-13 16:38:54 +0800

    细节优化

--- model/utils.js
+++ model/utils.js
@@ -1,12 +1,11 @@
 import NoteUser from '../../genshin/model/mys/NoteUser.js'
-import common from '../../../lib/common/common.js'
 
 const _getUid = async function (qq) {
   let user = await NoteUser.create(qq)
   return user.uid
 }
 
-const getUid = async function (e, set = false) {
+export const getUid = async function (e, set = false) {
   var qq = e
   var ise = false
   if (e && e.user_id) {
@@ -20,22 +19,3 @@ const getUid = async function (e, set = false) {
   return uid
 }
 
-const makeForwardMsg = async function (e, msg = [], dec = '') {
-  if (typeof(msg) === "string") {
-    if (msg == "") return false
-    return await common.makeForwardMsg(e, [msg], dec, msgsscr)
-  }
-  if (msg.length == 0) return false
-  if (onlyMsg || typeof(msg[0]) === "string") return await common.makeForwardMsg(e, msg, dec, msgsscr)
-  
-  var forward
-  if (e.isGroup) forward = await e.group.makeForwardMsg(msg)
-  else if (e.friend) forward = await e.friend.makeForwardMsg(msg)
-  else forward = Bot.makeForwardMsg(forwardMsg)
-  return forward
-}
-
-export {
-  getUid,
-  makeForwardMsg,
-}

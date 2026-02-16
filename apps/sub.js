d47ce90b340a67c4f44c6450c0d2f9558085a695
HDTianRu <3291691454@qq.com>
2026-02-16 10:50:31 +0800

    细节优化

--- apps/sub.js
+++ apps/sub.js
@@ -12,48 +12,56 @@ export class sub extends plugin {
         fnc: 'sub'
       }]
     })
+  }
 
-    async accept(e) {
-      const url = e.msg.match(regex)
-      if (url) e.reply(url.map(getSubscriptionInfo).filter(Boolean).join("\n"))
-    }
+  async accept(e) {
+    const url = e.msg.match(regex)
+    if (!url) return
+    e.reply(
+      (await Promise.all(url.map(getSubscriptionInfo).filter(Boolean))).join("\n")
+    )
+  }
 
-    async sub(e) {
-      
-    }
+  async sub(e) {
+
   }
 }
 
-async function getSubscriptionInfo(sub) {
-  const response = await fetch(sub, {
-    method: 'GET',
-    headers: {
-      'User-Agent': 'Mihomo/1.18.3'
-    }
-  })
-  if (!response.ok) return
-  const infoHeader = response.headers.get('subscription-userinfo')
-  if (!infoHeader) return
-  const data = {}
-  info.split('').forEach(item => {
-    const [key, value] = item.trim().split('=')
-    if (key && value) data[key] = parseInt(value)
-  })
 
-  const GB = 1024 * 1024 * 1024
-  const used = (data.upload + data.download) / GB
-  const total = data.total / GB
-  const remaining = total - used
-  const expire = data.expire ? new Date(data.expire * 1000).toLocaleString() : "永不过期"
-  /*return {
-    used,
-    total,
-    remaining,
-    expire
-  }*/
-  return `订阅信息:
+async function getSubscriptionInfo(sub) {
+  try {
+    const response = await fetch(sub, {
+      method: 'GET',
+      headers: {
+        'User-Agent': 'Mihomo/1.18.3'
+      }
+    })
+    if (!response.ok) return
+    const infoHeader = response.headers.get('subscription-userinfo')
+    if (!infoHeader) return
+    const data = {}
+    info.split('').forEach(item => {
+      const [key, value] = item.trim().split('=')
+      if (key && value) data[key] = parseInt(value)
+    })
+
+    const GB = 1024 * 1024 * 1024
+    const used = (data.upload + data.download) / GB
+    const total = data.total / GB
+    const remaining = total - used
+    const expire = data.expire ? new Date(data.expire * 1000).toLocaleString() : "永不过期"
+    /*return {
+      used,
+      total,
+      remaining,
+      expire
+    }*/
+    return `订阅信息:
 链接: ${sub}
 流量信息: ${used.toFixed(2)}/${total.toFixed(2)}
 剩余流量: ${remaining.toFixed(2)}
 过期时间: ${expire}`
+  } catch (e) {
+    return
+  }
 }

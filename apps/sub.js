17cbe8d31f1297f96736fb59705997cb6967b034
HDTianRu <3291691454@qq.com>
2026-02-19 20:32:09 +0800

    细节优化

--- apps/sub.js
+++ apps/sub.js
@@ -1,4 +1,5 @@
 import linkify_lib from "linkify-it"
+
 const linkify = linkify_lib()
 
 export class sub extends plugin {
@@ -18,9 +19,22 @@ export class sub extends plugin {
   async accept(e) {
     const url = urls(e.raw_message)
     if (!url) return null
-    e.reply(
-      (await Promise.all(url.map(getSubscriptionInfo).filter(Boolean))).join("\n").trim()
+    const infos = await Promise.all(
+      url.map(getSubscriptionInfo).filter(Boolean)
     )
+    for (const info of infos) {
+      const data = {
+        url: info.url,
+        used: formatStorage(info.used),
+        total: formatStorage(info.total),
+        expire: info.expire,
+        remainingPercent: Number(info.remaining / info.total).toFixed(1),
+        offset: (info.used / info.total) * 440
+      }
+      render('sub/index.html', data, {
+        e
+      })
+    }
   }
 
   /*async sub(e) {
@@ -40,20 +54,20 @@ function urls(text) {
 }
 
 function formatStorage(bytes) {
-    if (bytes <= 0) return '0 B';
-    const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
-    let i = 0;
-    let value = bytes;
-    while (value >= 1024 && i < units.length - 1) {
-        value /= 1024;
-        i++;
-    }
-    return `${parseFloat(value.toFixed(1))}${units[i]}`;
+  if (bytes <= 0) return '0 B';
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+  let i = 0;
+  let value = bytes;
+  while (value >= 1024 && i < units.length - 1) {
+    value /= 1024;
+    i++;
+  }
+  return `${parseFloat(value.toFixed(1))}${units[i]}`;
 }
 
-async function getSubscriptionInfo(sub) {
+async function getSubscriptionInfo(url) {
   try {
-    const response = await fetch(sub, {
+    const response = await fetch(url, {
       method: 'HEAD',
       headers: {
         'User-Agent': 'clash/clash.meta/Mihomo/1.18.3'
@@ -63,22 +77,23 @@ async function getSubscriptionInfo(sub) {
     const info = response.headers.get('subscription-userinfo')
     if (!info) return null
     const data = Object.fromEntries(info.split(";").map(i => i.trim().split('=')))
-    logger.mark(sub, data)
+    logger.mark(url, data)
     const used = Number(data.upload) + Number(data.download)
     const total = data.total
     const remaining = total - used
-    const expire = data.expire ? new Date(data.expire * 1000).toLocaleString() : "永不过期"
-    /*return {
+    const expire = data.expire ? new Date(data.expire * 1000).toLocaleString('zh-CN') : "永不过期"
+    return {
+      url,
       used,
       total,
       remaining,
       expire
-    }*/
-    return `订阅信息:
-链接: ${sub}
+    }
+    /*return `订阅信息:
+链接: ${url}
 已用流量: ${formatStorage(used)}/${formatStorage(total)}(${Number(used / total * 100).toFixed(1)}%)
 剩余流量: ${formatStorage(remaining)}(${Number(remaining / total * 100).toFixed(1)}%)
-过期时间: ${expire}`
+过期时间: ${expire}`*/
   } catch (e) {
     return null
   }
